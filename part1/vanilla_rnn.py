fc75c163352f84ae5b2e8a968674a5cd6e6ed99a
Emma <emsalinha@gmail.com>
2019-04-25 17:01:57 +0200

    debug model

--- part1/vanilla_rnn.py
+++ part1/vanilla_rnn.py
@@ -33,17 +33,17 @@ class VanillaRNN(nn.Module):
         self.batch_size = batch_size
         self.device = device
         self.seq_length = seq_length #10
-
+        self.num_hidden = num_hidden
         # input_dim 1
         # num_hidden 128
         # num_classes 10
         # batch_size 128
 
         # parameters
-        self.W_hx = nn.Parameter(Variable(torch.randn(input_dim, num_hidden), requires_grad = True)) #1 x 128
-        self.W_hh = nn.Parameter(Variable(torch.randn(num_hidden, num_hidden), requires_grad = True)) #128 x 128
-        self.bias_h = nn.Parameter(Variable(torch.zeros(num_hidden), requires_grad = True)) #128
-        self.W_ph = nn.Parameter(Variable(torch.randn(num_hidden, num_classes), requires_grad = True)) #128x10
+        self.W_hx = nn.Parameter(Variable(torch.randn(self.num_hidden, input_dim), requires_grad = True)) #1 x 128
+        self.W_hh = nn.Parameter(Variable(torch.randn(self.num_hidden, self.num_hidden), requires_grad = True)) #128 x 128
+        self.bias_h = nn.Parameter(Variable(torch.zeros(self.num_hidden), requires_grad = True)) #128
+        self.W_ph = nn.Parameter(Variable(torch.randn(self.num_hidden, num_classes), requires_grad = True)) #128x10
         self.bias_p = nn.Parameter(Variable(torch.zeros(num_classes), requires_grad = True)) #10
 
         self.h_zero = torch.zeros(num_hidden)
@@ -54,19 +54,34 @@ class VanillaRNN(nn.Module):
         self.hs = [self.h_zero]
         self.ys = []
 
-        for step in range(0, self.seq_length):
-            print(x.size())
-            xh = torch.matmul(x, self.W_hx)
+        for i in range(0, self.seq_length):
+            x_i = torch.reshape(x[:, i], shape=(1, self.num_hidden))
+            xh = torch.matmul(self.W_hx, x_i)
+            # print(xh.size())
             hh = torch.matmul(self.hs[-1], self.W_hh) + self.bias_h
+            # print(hh.size())
             h = torch.tanh(xh + hh)
+            # print(h.size())
             self.hs.append(h)
             p = torch.matmul(h,self.W_ph) + self.bias_p
+            # print(p.size())
             y = self.sigmoid(p)
             self.ys.append(y)
 
         return self.ys[-1]
 
     def sigmoid(self,x):
-        b = x.max(dim=1)
-        y = torch.exp(x.T - b)
-        return y / y.sum(dim=0)
+        # print(x.size())
+        b = x.max(dim=1)[0]
+        # print(b.size())
+        x = torch.transpose(x, -1, 0)
+        # print(x.size())
+        y = torch.exp(x - b)
+        # print(y.size())
+        y_sum = y.sum(dim=0)
+        # print(y_sum.size())
+        s = y / y.sum(dim=0)
+        s = torch.transpose(s, -1, 0)
+        print(s.size())
+        # 128 * 10
+        return s
