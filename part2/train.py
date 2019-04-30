273cbfcc04e59f3966668113dd7cd1dbfefde747
Emma <emsalinha@gmail.com>
2019-04-30 13:58:39 +0200

    implement model

--- part2/train.py
+++ part2/train.py
@@ -17,12 +17,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
 import time
 from datetime import datetime
 import argparse
-
-import numpy as np
+import random
 
 import torch
 import torch.optim as optim
@@ -32,34 +30,77 @@ from part2.dataset import TextDataset
 from part2.model import TextGenerationModel
 
 ################################################################################
+def get_accuracy(predictions, batch_targets):
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
+    """
+    i_pred = predictions.max(dim=-1)[1]
+
+    correct = (i_pred == batch_targets).float()
+    accuracy = correct.mean()
+
+    return accuracy
+
+def generate_sentence(dataset, model, seq_length):
+
+    char_idx = random.randint(0, dataset.vocab_size-1)
+    char_idxs = [char_idx]
+    char_tensor = torch.tensor(char_idx).view(1, 1, 1).float()
+
+    for i in range(0, seq_length):
+        prediction = model(char_tensor, batch = False)
+
+        i_preds = prediction.max(dim=-1)[1]
+        i_pred = i_preds[-1].item()
+        char_idxs.append(i_pred)
+
+        # sentence = dataset.convert_to_string(char_idxs)
+        # print(sentence)
+
+        char_tensor = torch.tensor(char_idxs).view(len(char_idxs), 1, 1).float()
+
+    sentence = dataset.convert_to_string(char_idxs)
+    print(sentence)
 
 def train(config):
 
     # Initialize the device which to run the model on
     device = torch.device(config.device)
 
-    # Initialize the model that we are going to use
-    model = TextGenerationModel( ... )  # fixme
-
-    # Initialize the dataset and data loader (note the +1)
-    dataset = TextDataset( ... )  # fixme
+    # Initialize the dataset and data loader
+    dataset = TextDataset(config.txt_file, config.seq_length, config.train_steps, config.batch_size)
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
 
+    # Initialize the model that we are going to use
+    model = TextGenerationModel(config.batch_size, config.seq_length, dataset.vocab_size,
+                 config.lstm_num_hidden, config.lstm_num_layers, config.device)
+
     # Setup the loss and optimizer
-    criterion = None  # fixme
-    optimizer = None  # fixme
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=config.learning_rate)
 
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
         # Only for time measurement of step through network
         t1 = time.time()
+        optimizer.zero_grad()
 
-        #######################################################
-        # Add more code here ...
-        #######################################################
+        batch_inputs = torch.unsqueeze(torch.stack(batch_inputs), 2).float()
+        batch_targets = torch.stack(batch_targets)
+        batch_targets = batch_targets.view(-1)
+        #batch_targets = batch_targets[-1, :]
 
-        loss = np.inf   # fixme
-        accuracy = 0.0  # fixme
+        batch_inputs, batch_targets = batch_inputs.to(device), batch_targets.to(device)
+
+        predictions = model(batch_inputs)
+
+        loss = criterion(predictions, batch_targets)
+        accuracy = get_accuracy(predictions, batch_targets)
+
+
+        loss.backward()
+        optimizer.step()
 
         # Just for time measurement
         t2 = time.time()
@@ -67,16 +108,17 @@ def train(config):
 
         if step % config.print_every == 0:
 
-            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
+            print("[{}] Train Step {}/{}, Batch Size = {}, Examples/Sec = {}, "
+                  "Accuracy = {}, Loss = {}".format(
                     datetime.now().strftime("%Y-%m-%d %H:%M"), step,
                     config.train_steps, config.batch_size, examples_per_second,
                     accuracy, loss
             ))
 
-        if step == config.sample_every:
+        if step % config.sample_every == 0:
             # Generate some sentences by sampling from the model
-            pass
+            generate_sentence(dataset, model, config.seq_length)
+            #pass
 
         if step == config.train_steps:
             # If you receive a PyTorch data-loader error, check this bug report:
@@ -95,7 +137,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--txt_file', type=str, required=True, help="Path to a .txt file to train on")
+    parser.add_argument('--txt_file', type=str, default='assets/book_EN_grimms_fairy_tails.txt', help="Path to a .txt file to train on")
     parser.add_argument('--seq_length', type=int, default=30, help='Length of an input sequence')
     parser.add_argument('--lstm_num_hidden', type=int, default=128, help='Number of hidden units in the LSTM')
     parser.add_argument('--lstm_num_layers', type=int, default=2, help='Number of LSTM layers in the model')
@@ -116,6 +158,8 @@ if __name__ == "__main__":
     parser.add_argument('--summary_path', type=str, default="./summaries/", help='Output path for summaries')
     parser.add_argument('--print_every', type=int, default=5, help='How often to print training progress')
     parser.add_argument('--sample_every', type=int, default=100, help='How often to sample from the model')
+
+    parser.add_argument('--device', type=str, default="cpu", help="Training device 'cpu' or 'cuda:0'")
 
     config = parser.parse_args()
 
